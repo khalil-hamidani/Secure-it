570a00bf75d6bb6e36207fcc2c4943ca93ea26de
khalil-hamidani <hamidani2002@gmail.com>
2022-12-27 14:28:47 +0100

    flask app step1

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 import os
+from flask import Flask, flash, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
 
-from flask import Flask, flash, jsonify, redirect, render_template, request ,session
 
 app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html",nav=True)
+
+
+@app.route("/encryption")
+def encryption():
+    return render_template("encryption.html",nav=True)
+
+
+@app.route("/decryption")
+def decryption():
+    return render_template("decryption.html",nav=True)
+
+
+@app.route("/passwordGen")
+def passwordGen():
+    return render_template("passwordsGen.html",nav=True)
+
+
 @app.route("/login")
 def login():
-
     return render_template("login.html")
 
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ helpers.py
@@ -0,0 +1,21 @@
+import os
+# import requests
+import urllib.parse
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
